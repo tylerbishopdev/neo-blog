b305d95669bb9aeb50a86dd11d70e7a2ac4ef463
Lance Ross <102563271+lancerossdev@users.noreply.github.com>
2022-11-30 05:16:50 +0000

    Initial commit and setup

---
+++ astro.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from "astro/config";
+import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
+import astroLayouts from "astro-layouts";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://nofuss.lanceross.xyz",
+  base: "/",
+  markdown: {
+    shikiConfig: {
+      theme: "dracula",
+    },
+  },
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+  integrations: [
+    compress({
+      css: true,
+      html: true,
+      js: true,
+      img: true,
+      svg: true,
+      logger: 0,
+    }),
+    tailwind(),
+    sitemap(),
+    mdx({
+      remarkPlugins: [
+        [
+          astroLayouts,
+          {
+            default: "@layouts/Layout.astro",
+            "pages/blog/**/*.mdx": "@layouts/BlogLayout.astro",
+          },
+        ],
+      ],
+    }),
+    image(),
+  ],
+});
