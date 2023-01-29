29a47b55d42d0f6e23bace733d83b463729bcf59
Lance Ross <102563271+lancerossdev@users.noreply.github.com>
2023-01-29 13:39:08 +0800

    lets try svgo ssr

--- astro.config.ts
+++ astro.config.ts
@@ -11,6 +11,11 @@ import codeTitle from "remark-code-title";
 export default defineConfig({
   site: "https://nofuss.pages.dev",
   base: "/",
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
   markdown: {
     shikiConfig: {
       theme: "slack-dark",
